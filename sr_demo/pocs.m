754b384889d1289ed31d4bf67988c7307163e63f
Grant Schwartz <gaschw3@cs.uky.edu>
2012-04-03 22:34:13 -0400

    pocs commented, no idea why you'd want to do those things

--- sr_demo/pocs.m
+++ sr_demo/pocs.m
@@ -1,11 +1,16 @@
 function image=pocs(image,lr_image,ds,sh)
 
+% shift image and reduce its size by factor of ds
 sh_image=gen_shift_downsample_image(image,ds,sh);
-scale=lr_image./sh_image;
-%         imshow(sh_image/255);
-% %imagesc(scale);
-% %         title(sprintf('tid: %d',tid));
-%         pause;
 
+%right division: A/B = A*B^-1
+scale=lr_image./sh_image;
+
+% a=kron(scale,ones(ds))
+%    expands scale to the output size
+% shift_image(a,-sh)
+%   shift a in the opposite direction of sh_image
 scale=shift_image(kron(scale,ones(ds)),-sh);
+
+% multiple every element by scale
 image=image.*scale;
