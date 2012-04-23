6a311f8f7dcb512b1146fcaa733330f762d417b7
Mitch <mitch.morton@gmail.com>
2012-04-22 23:12:37 -0400

    Updated the makefile for test_sr_one_step_wb_INC.  Sorry Grant.

--- src/test_sr_one_step_wb_INC/test_sr_one_step_wb.cpp
+++ src/test_sr_one_step_wb_INC/test_sr_one_step_wb.cpp
@@ -25,46 +25,48 @@ int main(int argc, char* argv[]){
     cout << "Usage: functioncall ImagetoUse" << endl;
     return -1;
     }*/
-int rescale_factor = 4;
-input_image image;
-input_image curr;
-vector <input_image> input;
-curr.file = imread("1.jpg");
-imshow("pop", curr.file);
-resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
-curr.file = shiftMat(curr.file, -3, 4);
-input.push_back(curr);
-curr.file = imread("1.jpg");
-resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
-curr.file = shiftMat(curr.file, -6, 0);
-input.push_back(curr);
-curr.file = imread("1.jpg");
-resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
-curr.file = shiftMat(curr.file, 3, 4);
-input.push_back(curr);
-curr.file = imread("1.jpg");
-resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
-curr.file = shiftMat(curr.file, 0, -4);
-input.push_back(curr);
-curr.file = imread("1.jpg");
-resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
-curr.file = shiftMat(curr.file, 5, -2);
-input.push_back(curr);
-cout << "third" << endl;
-image.file = imread("1.jpg");
+	int rescale_factor = 4;
+	input_image image;
+	input_image curr;
+	vector <input_image> input;
+	curr.file = imread("../coffee.jpg");
+	//imshow("pop", curr.file);	
+	resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
+	curr.file = shiftMat(curr.file, -3, 4);
+	input.push_back(curr);
+	curr.file = imread("../coffee.jpg");
+	resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
+	curr.file = shiftMat(curr.file, -6, 0);
+	input.push_back(curr);
+	curr.file = imread("../coffee.jpg");
+	resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
+	curr.file = shiftMat(curr.file, 3, 4);
+	input.push_back(curr);
+	curr.file = imread("../coffee.jpg");
+	resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
+	curr.file = shiftMat(curr.file, 0, -4);
+	input.push_back(curr);
+	curr.file = imread("../coffee.jpg");
+	resize(curr.file, curr.file, Size(), 4, 4, CV_INTER_AREA);
+	curr.file = shiftMat(curr.file, 5, -2);
+	input.push_back(curr);
+	cout << "third" << endl;
+	image.file = imread("../coffee.jpg");
 
+	Mat kron_image;
+	//resize(image.file, image.file, Size(), 4, 4, CV_INTER_AREA);
 
-Mat kron_image;
-//resize(image.file, image.file, Size(), 4, 4, CV_INTER_AREA);
+	Mat output = sr_one_step_wb(image, input);
+	cout << "working" << endl;
+	for(int i = 0; i<input.size(); i++){
+		cout << "horizontal shift: " << input[i].horizontal_shift;
+		cout << ", vertical shift: " << input[i].vertical_shift << endl;
+		cout << "prob" << input[i].prob << endl;
+	}
 
-Mat output = sr_one_step_wb(image, input);
-cout << "working" << endl;
-for(int i = 0; i<input.size(); i++){
-	cout << "horizontal shift: " << input[i].horizontal_shift;
-	cout << ", vertical shift: " << input[i].vertical_shift << endl;
-}
-
-system("pause");
+	imshow("output", output);
+	waitKey(0);
+	system("pause");
 	return 0;
 }
 
