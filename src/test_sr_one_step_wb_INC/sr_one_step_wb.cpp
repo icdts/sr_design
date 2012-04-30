4bc142f2364e37357841ddfd65f231c5d24a5380
Casey <Casey@Casey-PC.(none)>
2012-04-29 23:02:36 -0400

    sr_one_step.h

--- src/test_sr_one_step_wb_INC/sr_one_step_wb.cpp
+++ src/test_sr_one_step_wb_INC/sr_one_step_wb.cpp
@@ -1,18 +1,11 @@
-#include <opencv/cv.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include <vector>
-#include "../final/image_manipulations.h"
-#include "../final/load_images.h"
-#include "../test_subpixel_register/subpixel_register.h"
+#include "sr_one_step.h"
 
 using namespace std;
 using namespace cv;
 
 /* This function calls the subpixel_register function and creates the final super-resolution output. 
 It is given a source image (mainly for dimensions?) and the vector of input images, and returns a Mat.*/
-Mat sr_one_step(input_image &src, vector <input_image> &input);
+
 
 /* This function calls the subpixel_register function and creates the final super-resolution output. 
 It is given a source image (mainly for dimensions?) and the vector of input images, and returns a Mat.*/
