1392041e2b36150267fb907a02bb65b0405dbeb2
Robbie Nolan <mooble.marble@gmail.com>
2012-04-29 23:27:31 -0400

    sr_gui compiles

--- src/subpixel_register.h
+++
@@ -1,5 +0,0 @@
-#include <opencv/cv.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
-float subpixel_register(input_image *hr_image, input_image *lr_image, int ds, int sigma); 
