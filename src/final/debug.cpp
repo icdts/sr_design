3327056b8a2bd30f7a100c231281971c304f1c26
James Morton <jmort2@Murphy.local>
2012-04-29 20:06:03 -0400

    Added printMatrix.

--- src/final/debug.cpp
+++ src/final/debug.cpp
@@ -1,9 +1,27 @@
 #include "debug.h"
 
 using namespace std;
+using namespace cv;
 
-void debug(std::string str){
+void debug(string str){
 #ifdef DEBUG
 	cerr << "DEBUG: " << str << endl;
 #endif
+}
+
+void printMatrix(Mat m){
+	vector<Mat> panels;
+	
+	split(m, panels);
+
+	for(int channels = 0; channels < m.channels(); channels++){
+		for(int row = 0; row < m.rows; row++){
+			cout << "[";
+			for(int col = 0; col < m.cols; col++){
+				cout << panels[channels].at<float>(row, col)<<"\t";
+			}
+			cout << "]\n";
+		}
+		cout << "\n\n";
+	}
 }
--- src/final/debug.h
+++ src/final/debug.h
@@ -1,6 +1,14 @@
+#ifdef _MSC_VER
+#else
+	#include <opencv/cv.h>
+	#include <opencv2/core/core.hpp>
+	#include <opencv2/highgui/highgui.hpp>
+#endif
 #include <iostream>
 #include <string>
+#include <vector>
 
 #define DEBUG
  
 void debug(std::string);
+void printMatrix(cv::Mat);
